a5db94200504ffad2d09ed9b42ff6f78d7c2c314
hgabka <hgabka@gmail.com>
2021-12-11 08:01:10 +0100

    Update AdminControllerListener.php

--- src/EventListener/AdminControllerListener.php
+++ src/EventListener/AdminControllerListener.php
@@ -1,1 +1,33 @@
+<?php
+
+namespace Hgabka\NodeBundle\EventListener;
+
+use Hgabka\NodeBundle\Controller\NodeAdminController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class AdminControllerListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::CONTROLLER => ['onKernelController', 10]];
+    }
+
+    public function onKernelController(ControllerEvent $event): void
+    {
+        dump(1000);
+        $controller = $event->getController();
+
+        if (\is_array($controller)) {
+            $controller = $controller[0];
+        }
+
+        if ($controller instanceof NodeAdminController) {
+            $request = $event->getRequest();
+            
+            $request->query->set('_sonata_admin', 'hgabka_node.admin.node');
+        }
+    }
+}
 
