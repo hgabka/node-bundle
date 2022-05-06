79f7d50e26ec1c11de86f43549899efbdd9efd11
hgabka <hgabka@gmail.com>
2022-05-06 13:40:48 +0200

    Update ConvertSequenceNumberToWeightCommand.php

--- src/Command/ConvertSequenceNumberToWeightCommand.php
+++ src/Command/ConvertSequenceNumberToWeightCommand.php
@@ -37,7 +37,7 @@ class ConvertSequenceNumberToWeightCommand extends Command
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $em = $this->entityManager;
 
