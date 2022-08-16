7fa479d7b9a15ee879d2bb7e2afae7b00c33530f
Gabe <hgabka@gmail.com>
2022-08-16 19:50:13 +0200

    fix

--- src/AdminList/FilterType/NodeSearchFilterType.php
+++ src/AdminList/FilterType/NodeSearchFilterType.php
@@ -46,7 +46,13 @@ class NodeSearchFilterType extends AbstractORMFilterType
                 $ids[] = $nt->getNode()->getId();
             }
 
-            $this->queryBuilder->andWhere($this->getAlias() . 'id IN (:ids_' . $uniqueId . ')')->setParameter('ids_' . $uniqueId, $ids);
+            if (!empty($ids)) {
+                $this->queryBuilder->andWhere($this->getAlias() . 'id IN (:ids_' . $uniqueId . ')')->setParameter('ids_' . $uniqueId, $ids);
+            } else {
+                $this->queryBuilder->andWhere('1=0');
+            }
+        } else {
+            $this->queryBuilder->andWhere('1=0');
         }
     }
 
