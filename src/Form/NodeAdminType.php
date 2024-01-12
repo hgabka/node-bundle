a73ad3a0f5a5e4861f491614e85ea03c30d00f8f
hgabka <hgabka@gmail.com>
2024-01-12 14:52:17 +0100

    Update NodeAdminType.php

--- src/Form/NodeAdminType.php
+++ src/Form/NodeAdminType.php
@@ -13,7 +13,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class NodeAdminType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('id', HiddenType::class);
     }
@@ -26,7 +26,7 @@ class NodeAdminType extends AbstractType
         return 'node';
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Node::class,
